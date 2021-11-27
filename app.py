2dce53ccbafa2e67af5ed8c303b69d453c1f5251
jemin <jemin03120111@gmail.com>
2021-11-28 03:47:42 +0900

    open api 테스트

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ class NuguApi(Resource):
       rescode = response.getcode()
       if(rescode==200):
          response_body = json.loads(response.read())
-         print(response_body.items);
+         print(response_body.items[0]);
       else:
          print("Error Code:" + rescode)
       data =  {
