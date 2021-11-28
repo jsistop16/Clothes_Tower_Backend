5585abff396ac844b568c949e0edbac477354eff
jemin <jemin03120111@gmail.com>
2021-11-28 16:57:20 +0900

    weather api

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ class NuguApi(Resource):
          
          # 기상예보 서비스 
       url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
-      params ={'serviceKey' : os.environ.get("WEATHER_KEY"), 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'JSON', 'base_date' : '20210628', 'base_time' : '0600', 'nx' : '55', 'ny' : '127' }
+      params ={'serviceKey' : os.environ.get("WEATHER_KEY"), 'pageNo' : '1', 'numOfRows' : '1000', 'dataType' : 'JSON', 'base_date' : '20211128', 'base_time' : '0600', 'nx' : '55', 'ny' : '127' }
       response = requests.get(url, params=params)
       print(response.json());
          
