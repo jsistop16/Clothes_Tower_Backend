29ec5f2228d9693ca042937fcbf771758b469fc8
jemin <jemin03120111@gmail.com>
2021-12-02 10:29:10 +0900

    .

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ db.init_app(app);
 #===== REST API 세팅하는 과정 =======
 
 api = Api(app);
-api.add_namespace(Clothes, '/clothes')      # backend 
-api.add_namespace(NuguSpeacker, '/nugu')    # nugu speaker
+# api.add_namespace(Clothes, '/clothes')      # backend 
+# api.add_namespace(NuguSpeacker, '/nugu')    # nugu speaker
 
 if __name__ == "__main__":
     
