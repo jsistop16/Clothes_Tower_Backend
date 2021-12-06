8598ef51e03e42dc4230c4580642c39e05a82d5d
jemin <jemin03120111@gmail.com>
2021-12-07 03:06:41 +0900

    color 리팩터링

--- NUGU/nuguRoute.py
+++ NUGU/nuguRoute.py
@@ -95,7 +95,7 @@ class Image(Resource):
     result = run_vision("./upload/test.png");
     result2 = result.dominant_colors.colors[0].color;
     colorResult = pickColor(int(result2.red),int(result2.green),int(result2.blue));
-    if colorResult == nullcontext :
+    if colorResult == None :
        print("다시 색상을 인식시키세요")
        return "잘못된 입력값입니다"
     else :
